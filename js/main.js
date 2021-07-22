fe07153c624cf53271366cb750f51128210296e1
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-22 16:14:16 +0200

    commit iniziale

---
+++ js/main.js
@@ -0,0 +1,2 @@
+/*Proviamo a ripetere quanto visto in classe, creando la struttura che riteniamo più adeguata per rappresentare una carta di Magic.
+Una volta definita la struttura, stampiamo sulla pagina HTML tutte le informazioni relative alla carta stessa, senza particolare attenzione a dettagli grafici (va bene una lista coi tag UL e LI) */
