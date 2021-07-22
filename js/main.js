7985418fdd3de696a8713699402b9049a62a733a
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-22 17:07:54 +0200

    element object

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 /*Proviamo a ripetere quanto visto in classe, creando la struttura che riteniamo più adeguata per rappresentare una carta di Magic.
 Una volta definita la struttura, stampiamo sulla pagina HTML tutte le informazioni relative alla carta stessa, senza particolare attenzione a dettagli grafici (va bene una lista coi tag UL e LI) */
+
+
+//* Inizializzazzione dell'oggetto
+
+const card = {
+    nome: '',
+    cost: ['6', 'R', 'R'],
+    tipo: 'Creatura',
+    specificita: 'Ninja',
+    espansione: {
+        expNumber: 12,
+        rarity: 'silver',
+    },
+    abilita: [   //Array di oggetti
+        {
+            costoAbilita: ['R', 'T'],
+            descrizione: 'Descrizione carta',
+        }
+    ]
+
+}
