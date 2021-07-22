d9a23a09961f3fae7e87fdf8b2fa0002bf93e460
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-22 17:21:46 +0200

    descrizione oggetto completa

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 /*Proviamo a ripetere quanto visto in classe, creando la struttura che riteniamo più adeguata per rappresentare una carta di Magic.
 Una volta definita la struttura, stampiamo sulla pagina HTML tutte le informazioni relative alla carta stessa, senza particolare attenzione a dettagli grafici (va bene una lista coi tag UL e LI) */
 
+// --------------------------------------------- Svolgimento -------------------------------------------//
+
+// recupero elemento html
+let cardDisplay = document.getElementById('card-display');
 
 //* Inizializzazzione dell'oggetto
-
 const card = {
     nome: '',
     cost: ['6', 'R', 'R'],
@@ -13,11 +16,17 @@ const card = {
         expNumber: 12,
         rarity: 'silver',
     },
-    abilita: [   //Array di oggetti
-        {
-            costoAbilita: ['R', 'T'],
-            descrizione: 'Descrizione carta',
-        }
-    ]
-
+    abilita: {
+        costoAbilita: ['R', 'T'],
+        descrizione: 'Descrizione carta',
+    },
+    flavorText: {
+        testo: 'Lorem....',
+        autore: 'Ddhcbcibe'
+    },
+    autore: 'ciaociao',
+    numeroCollezione: '127/350',
+    forza: '13',
+    costituzione: '13',
+    bordo: '#12345'
 }
